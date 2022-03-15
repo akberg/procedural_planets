922497517986061d48792cfd7d458224acd9a502
Andreas K. Berg <andreaskb98@gmail.com>
2022-03-15 08:57:16 +0100

    start charmap, change glm -> nalgebra-glm

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use glium::{Surface};
 
 use std::sync::{Arc, Mutex};
 
-use glm::noise2;
+use nalgebra_glm as glm;
 
 
 #[derive(Copy, Clone)]
@@ -21,6 +21,10 @@ fn main() {
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb,cb, &event_loop).unwrap();
+
+    // Charmap
+    let charmap = image::io::Reader::open("resources/textures/charmap.png").unwrap().decode().unwrap();
+    // image::load(std::io::Cursor::new(&include_bytes!("resources/textures/charmap.png")), image::ImageFormat::Png).unwrap().to_rgba8();
 
     // Load shaders
     let vertex_shader_src = std::fs::read_to_string("resources/shaders/planet.vert").unwrap();
@@ -93,4 +97,27 @@ fn main() {
         // Finish drawing
         target.finish().unwrap();
     });
+}
+
+
+fn generate_text_geometry_buffer(text: &str, char_h_over_w: f32, total_text_w: f32) {
+    let char_w = total_text_w / text.len() as f32;
+    let char_h = char_h_over_w * char_w;
+
+    let vertex_count = 4 * text.len();
+    let index_count = 6 * text.len();
+
+    let mut vertices = vec![glm::vec3(0.0f32, 0.0, 0.0); vertex_count];
+    let mut uv_coords = vec![glm::vec2(0.0f32, 0.0); vertex_count];
+    let mut indices = vec![glm::vec3(0u16, 0, 0); index_count];
+    let mut normals = vec![glm::vec3(0.0f32, 0.0, 0.0); vertex_count];
+
+    for (i, c) in text.chars().enumerate() {
+        let base_coord = i as f32 * char_w;
+        vertices[4 * i + 0] = glm::vec3(base_coord as f32, 0.0, 0.0);
+        vertices[4 * i + 1] = glm::vec3(base_coord as f32 + char_w, 0.0, 0.0);
+        vertices[4 * i + 2] = glm::vec3(base_coord as f32 + char_w, char_h, 0.0);
+        
+        vertices[4 * i + 3] = glm::vec3(base_coord as f32, char_h, 0.0);
+    }
 }
